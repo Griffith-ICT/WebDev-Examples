58b63779d92992346726cf6ef7269ec5a79c7f45
David Chen <d.chen@griffith.edu.au>
2017-04-11 02:21:42 +0000

    week4 & Week6

---
+++ week4/assoc-laravel/routes/web.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+require(app_path().'/pms.php');  // The file containing the pms array is in the app directory. 
+                                 // app_path() give us the path the the app directory
+
+// To do: Display search form
+Route::get('/', function()
+{
+  // Remove the line below, and implement your own code.
+	dd("Welcome to PMS, there is a lot to do, so let's get on with it");
+});
+
+
+// To do: Perform search and display results
+Route::get('search', function()
+{
+  // Fetch form data
+
+  // call search() to perform search
+
+  // call view to display search result
+});
+
+
+/* Functions for PM database example. */
+
+/* Search sample data for $name or $year or $state from form. */
+function search($name, $year, $state) {
+  $pms = getPms();
+
+  // Filter $pms by $name
+  if (!empty($name)) {
+    $results = array();
+    foreach ($pms as $pm) {
+      if (stripos($pm['name'], $name) !== FALSE) {
+        $results[] = $pm;
+      }
+    }
+    $pms = $results;
+  }
+
+  // Filter $pms by $year
+  if (!empty($year)) {
+    $results = array();
+    foreach ($pms as $pm) {
+      if (strpos($pm['from'], $year) !== FALSE || 
+          strpos($pm['to'], $year) !== FALSE) {
+        $results[] = $pm;
+      }
+    }
+    $pms = $results;
+  }
+
+  // Filter $pms by $state
+  if (!empty($state)) {
+    $results = array();
+    foreach ($pms as $pm) {
+      if (stripos($pm['state'], $state) !== FALSE) {
+        $results[] = $pm;
+      }
+    }
+    $pms = $results;
+  }
+
+  return $pms;
+}
---
+++ week6/create_item_table.sql
@@ -0,0 +1,11 @@
+drop table if exists item;
+create table item (    
+    id integer not null primary key autoincrement,    
+    summary varchar(80) not null,    
+    details text default '‘
+); 
+insert into item values (null, "Canon PowerShot S110",  "Excellent pocket camera.");
+insert into item values (null, "Canon EOS 700D", "Excellent entry-level digital SLR camera");
+insert into item values (null, "Canon EOS 7D",  "Excellent if aging mid-range digital SLR camera.");
+insert into item values (null, "Canon EOS 6D", "Excellent modern full-frame semi-professional digital SLR camera.");
+insert into item values (null, "Fujifilm X-Pro1", "Outstanding compact mirrorless system camera");
